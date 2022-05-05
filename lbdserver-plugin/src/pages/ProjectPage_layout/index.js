6021d47c4cd1461a2e22375b748db55e12e31f92
simonbaecke <simon.baecke@gmail.com>
2022-05-05 18:15:41 +0200

    projectpage expanded

--- lbdserver-plugin/src/pages/ProjectPage_layout/index.js
+++ lbdserver-plugin/src/pages/ProjectPage_layout/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import BasicTabs from '../Documentation/Dialogs/BasicTabs';
@@ -9,71 +9,33 @@ import CloseIcon from '@mui/icons-material/Close';
 import { IconButton, Tooltip } from '@mui/material';
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { project as p } from "../../atoms"
-import {newEngine} from '@comunica/actor-init-sparql'
+import { newEngine } from '@comunica/actor-init-sparql'
 import { getDefaultSession } from '@inrupt/solid-client-authn-browser';
-import {LbdProject } from 'lbdserver-client-api'
+import { LbdProject } from 'lbdserver-client-api'
 
 
 
 const ProjectPage = () => {
-  const [metadata, setMetadata] = useState({})
-  const [project, setProject] = useRecoilState(p)
-  
-  useEffect(() => {
-    getProjectData()
-    console.log('project', project)
-  }, [])
+    let navigate = useNavigate();
+    function handleClick() {
+        navigate("/dashboard")
+    }
 
-  let navigate = useNavigate();
-  function handleClick() {
-    navigate("/dashboard")
-  }
-
-  async function getProjectData() {
-      // https://comunica.dev/
-      const query =`SELECT ?label ?year ?country ?city ?currentStatus ?role ?id WHERE {
-          <${project.accessPoint}> <http://www.w3.org/2000/01/rdf-schema#label> ?label ;
-          <http://www.w3.org/2006/time#year> ?year ;
-          <http://dbpedia.org/ontology/country> ?country ;
-          <http://dbpedia.org/ontology/city> ?city ;
-          <http://dbpedia.org/ontology/currentStatus> ?currentStatus ;
-          <http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#Role> ?role ;
-          <http://id> ?id .       
-          
-      } LIMIT 1
-      `
-      const myEngine = newEngine()
-      const results = await myEngine.query(query, {sources: [project.accessPoint], fetch: getDefaultSession().fetch})
-      const bindings = await results.bindings()
-      bindings.forEach(binding => {
-          const myMetadata = {
-              label: binding.get('?label').id.slice(1, -1),
-              year: binding.get('?year').id.slice(1, -1),
-              country: binding.get('?country').id.slice(1, -1),
-              city: binding.get('?city').id.slice(1, -1),
-              currentStatus: binding.get('?currentStatus').id.slice(1, -1),
-              role: binding.get('?role').id.slice(1, -1),
-              id: binding.get('?id').id.slice(1, -1)
-          }
-          setMetadata(myMetadata);;
-      })
-  }
     return (
         <Grid container spacing={3} sx={{ p: 3 }}>
             <Grid item xs={8}>
                 <BasicTabs title={"Scroll through the enrichment modules, visualise and query the project."} />
             </Grid>
             <Grid item xs={3}>
-                <p>{metadata.label} </p>
+                <Info />
                 <FormDialog />
             </Grid>
             <Grid item xs={1} container justify="flex-end">
                 <Tooltip title="Close">
-                    <IconButton>
-                        <CloseIcon onClick={() => handleClick()}/>
+                    <IconButton onClick={() => handleClick()}>
+                        <CloseIcon />
                     </IconButton>
                 </Tooltip>
-
             </Grid>
         </Grid>
 
